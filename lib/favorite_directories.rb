013a214b362a02f0681f579f4b031c36a88142b0
Hiroaki Muronaka <hiroaki.muronaka@plus-add.com>
2015-08-25 18:46:18 +0900

    working

---
+++ lib/favorite_directories.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+DIRS_PATH=File.expand_path("~/.favorite_directories", __FILE__)
+
+def load_dirs
+  load_with_path(DIRS_PATH)
+end
+
+def load_dirs_with_path(yaml_path)
+  yaml_path = File.expand_path(yaml_path)
+
+  directories = []
+  begin
+    str = File.read(yaml_path)
+    directories = YAML.load(str)
+  rescue => e
+  end
+
+  directories
+end
+
+def save_dirs(dirs)
+  save_dirs_with_path(dirs, DIRS_PATH)
+end
+
+def save_dirs_with_path(dirs, path)
+  yaml_path = File.expand_path(path)
+
+  File.open(yaml_path, 'w') do |f|
+    f.write dirs.to_yaml
+  end
+end
